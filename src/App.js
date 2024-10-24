8bef173f1c45dfa9df1d89c5cbd4198baad43cf2
Christophe Carbin <blakehziir@gmail.com>
2024-10-24 18:27:58 +0200

    correction page home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Inscription from './utilisateur/inscription';
 import Navbar from './include/navbar';
